cd3483ea215fcaaa28eebb299596e54c45360392
AliCelikay <alicelikay129@gmail.com>
2023-01-13 12:05:01 -0600

    adding dark/light mode themes

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import RightBar from "./components/rightbar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 
+import './style.scss';
+
 function App() {
   const currentUser = true;
 
   const Layout = () => {
     return (
-      <div>
+      <div className="theme-dark">
         <Navbar/>
         <div style={{ display: "flex"}}>
           <LeftBar/>
