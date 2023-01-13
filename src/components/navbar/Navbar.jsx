331fd54b6b786e4dbddbed18812181158e0205bf
AliCelikay <alicelikay129@gmail.com>
2023-01-13 13:02:18 -0600

    added toggle functionality to components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,35 +7,42 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
 import { Link } from 'react-router-dom';
+import { DarkModeContext } from '../../context/darkModeContext';
+import { useContext } from 'react';
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-        <div className='left'>
-            <Link to="/" style={{ textDecoration: 'none'}}>
-                <span>AliSocial</span>
-            </Link>
-            <HomeOutlinedIcon/>
-            <DarkModeOutlinedIcon/>
-            <GridViewOutlinedIcon/>
-            <div className='search'>
-                <SearchOutlinedIcon/>
-                <input type="text" placeholder="Search..."/>
-            </div>
-        </div>
+    // destructuring toggle function from the DarkModeContext...
+    const {toggle} = useContext(DarkModeContext);
 
-        <div className='right'>
-            <PersonOutlinedIcon/>
-            <EmailOutlinedIcon/>
-            <NotificationsOutlinedIcon/>
-            <div className="user">
-                <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                <span>John Doe</span>
+    return (
+        <div className="navbar">
+            <div className='left'>
+                <Link to="/" style={{ textDecoration: 'none' }}>
+                    <span>AliSocial</span>
+                </Link>
+                <HomeOutlinedIcon />
+                {/* ... and using the toggle function in the icon with onClick event */}
+                <DarkModeOutlinedIcon onClick={toggle} />
+                <GridViewOutlinedIcon />
+                <div className='search'>
+                    <SearchOutlinedIcon />
+                    <input type="text" placeholder="Search..." />
+                </div>
+            </div>
+
+            <div className='right'>
+                <PersonOutlinedIcon />
+                <EmailOutlinedIcon />
+                <NotificationsOutlinedIcon />
+                <div className="user">
+                    <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                    <span>John Doe</span>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
